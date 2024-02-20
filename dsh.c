840165be3f7538f67a3d760c68f5eb8a14764995
Jia <jgu@>
2024-02-20 06:24:46 +0000

    simple_version

--- dsh.c
+++ dsh.c
@@ -23,6 +23,63 @@
  * This is just an example. Delete this before 
  * submission.
  */
+/*
 void example(int* x) {
     *x = thisIsGlobal;
-} 
+} */
+
+
+
+int split_length(char *str, char *delim){
+    char *s = strdup(str);   
+    int num_tokens=0;
+    int local_num =0;
+    char *token;
+
+    //allocate the array pointer of string 
+
+    for (token = strtok(s, delim); token != NULL; token = strtok(NULL, delim)) {
+        num_tokens++;
+    }
+    free(s);
+    return num_tokens;
+}
+
+
+
+
+char** split(char *str, char *delim){
+    char *s = strdup(str);   // copy of original string  https://www.geeksforgeeks.org/fgets-gets-c-language/
+    int num_tokens=0;
+    int local_num =0;
+    char *token;
+
+    //allocate the array pointer of string 
+
+    for (token = strtok(s, delim); token != NULL; token = strtok(NULL, delim)) {
+        num_tokens++;
+    }
+    free(s);
+    char **array = (char**) malloc((num_tokens+1) * sizeof(char*));
+    
+    //allocate the space for the string to which the array pointer points 
+    for (int i = 0; i < num_tokens+1; i++) {
+    array[i] = (char*) malloc( MAXBUF* sizeof(char));
+    }
+    //  the splited part 
+    token = strtok(str, delim);
+
+    s = strdup(str);  
+    while (token!=NULL) {
+        strcpy(array[local_num], token);
+        local_num=local_num+1;
+        token = strtok(NULL, delim); // contiune at the last place 
+    }
+    
+    free(s);
+    array[num_tokens]='\0';
+    return array;
+}
+
+
+
